f05df1b29dcab6cfacdceee7f7869d11a713e624
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-13 12:44:56 +0430

    init

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,6 +1,7 @@
-const { parse } = require("csv-parse");
 const fs = require("fs");
 const path = require("path");
+const { parse } = require("csv-parse");
+
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
@@ -24,7 +25,9 @@ function isHabitablePlanet(planet) {
 
 function loadPlanetsData() {
   return new Promise((resolve, reject) => {
-    fs.createReadStream("kepler_data.csv")
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
       .pipe(
         parse({
           comment: "#",
