312be7eb4174bf18078b5ba3b43c57e168367e0a
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-13 12:30:30 +0430

    init

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,3 +1,9 @@
 const { launches } = require("../../models/launches.model");
 
-function getAllLaunches(req, res) {}
+function getAllLaunches(req, res) {
+  return res.status(200).json(Array.from(launches.values()));
+}
+
+module.exports = {
+  getAllLaunches,
+};
