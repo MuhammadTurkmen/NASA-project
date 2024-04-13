c5066c99aab7b123bd4ff3a5b34e482e204ae163
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-13 09:53:26 +0430

    init

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,8 @@
 const http = require("http");
-const express = require("express");
-
-const app = express();
+const app = require("./app");
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
