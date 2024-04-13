d6587d78ed033ec5067ff03121f6d2327cc86b02
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-13 17:46:59 +0430

    init

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 const app = express();
