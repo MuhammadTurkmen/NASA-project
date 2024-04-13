fcfab9eb122bc9abb2d03156b96b4e657357f37f
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-13 10:09:18 +0430

    init

--- server/src/app.js
+++ server/src/app.js
@@ -3,5 +3,6 @@ const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
 const app = express();
 app.use(express.json());
+app.use(planetsRouter);
 
 module.exports = app;
---
+++ server/src/routes/planets/planets.controller.js
@@ -0,0 +1,3 @@
+const planets = [];
+
+function getAllPlanets(req, res) {}
