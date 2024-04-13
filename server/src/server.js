9d7c72001743d2f2675370ccc88d5d2b80e9b95f
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-13 12:39:54 +0430

    init

--- server/src/server.js
+++ server/src/server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+await loadPlanetsData();
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
